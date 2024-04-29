a9e0808d64f9d269ef3e2e181724083c9f05b07b
R-salton <ringusa22@gmail.com>
2024-04-29 11:06:59 +0200

    displaying messages using Stream

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -68,51 +68,109 @@ class _ChatScreenState extends State<ChatScreen> {
               }),
         ],
         title: const Text('⚡️Chat'),
-        backgroundColor: Colors.lightBlueAccent,
+        backgroundColor: kBlueScent,
       ),
       body: ModalProgressHUD(
         inAsyncCall: showSpinner,
         child: SafeArea(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Container(
-                decoration: kMessageContainerDecoration,
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    Expanded(
-                      child: TextField(
-                        style: const TextStyle(color: Colors.white),
-                        onChanged: (value) {
-                          //Do something with the user input.
-                          textMessage = value;
-                        },
-                        decoration: kMessageTextFieldDecoration,
-                      ),
-                    ),
-                    TextButton(
-                      onPressed: () {
-                        //Implement send functionality.
-                        try {
-                          _firestore.collection('messages').add({
-                            'sender': loggedInUser.email,
-                            'text': textMessage
-                          });
-                        } catch (e) {
-                          print(e);
+          child: Container(
+            color: kBackgroundDarkBlue,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                StreamBuilder<QuerySnapshot>(
+                    stream: _firestore.collection('messages').snapshots(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        final messages = snapshot.data?.docs;
+                        print(messages);
+                        List<MessageBubble> messageBubbles = [];
+                        for (var message in messages!) {
+                          final messageText = message['text'];
+                          final messageSender = message["sender"];
+                          final messageBubble = MessageBubble(
+                            sender: messageSender,
+                            message: messageText,
+                          );
+
+                          messageBubbles.add(messageBubble);
                         }
-                      },
-                      child: const Text(
-                        'Send',
-                        style: kSendButtonTextStyle,
+
+                        return Expanded(
+                          child: ListView(
+                            children: messageBubbles,
+                          ),
+                        );
+                      } else {
+                        return const Center(
+                          child: CircularProgressIndicator(
+                            backgroundColor: kBackgroundDarkBlue,
+                          ),
+                        );
+                      }
+                    }),
+                Container(
+                  decoration: kMessageContainerDecoration,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Expanded(
+                        child: TextField(
+                          style: const TextStyle(color: Colors.white),
+                          onChanged: (value) {
+                            //Do something with the user input.
+                            textMessage = value;
+                          },
+                          decoration: kMessageTextFieldDecoration,
+                        ),
                       ),
-                    ),
-                  ],
+                      TextButton(
+                        onPressed: () {
+                          //Implement send functionality.
+                          try {
+                            _firestore.collection('messages').add({
+                              'sender': loggedInUser.email,
+                              'text': textMessage
+                            });
+                          } catch (e) {
+                            print(e);
+                          }
+                        },
+                        child: const Text(
+                          'Send',
+                          style: kSendButtonTextStyle,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class MessageBubble extends StatelessWidget {
+  const MessageBubble({super.key, this.message, this.sender});
+  final message;
+  final sender;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Material(
+        borderRadius: BorderRadius.circular(30.0),
+        color: Colors.blueGrey,
+        elevation: 5.0,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+          child: Text(
+            "$message from $sender",
+            style: const TextStyle(fontSize: 15.0),
           ),
         ),
       ),
